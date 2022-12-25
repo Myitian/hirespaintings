e8cb6927ee235ac05d865a906e7b2be4bd4c879e
Myitian <miaoyitian233@gmail.com>
2022-12-26 01:22:06 +0800

    clean code & update atlas creating code & add HiResPaintingsMotive.toString() & update version to 1.2.0

--- src/main/java/com/myitian/hirespaintings/HiResPaintingsClient.java
+++ src/main/java/com/myitian/hirespaintings/HiResPaintingsClient.java
@@ -28,9 +28,8 @@ public class HiResPaintingsClient implements ClientModInitializer, ClientLifecyc
 
     @Override
     public void onClientStarted(MinecraftClient client) {
-        MinecraftClient mc = MinecraftClient.getInstance();
-        HIRESPAINTING_MANAGER = new HiResPaintingManager(mc.getTextureManager());
-        ((ReloadableResourceManagerImpl) mc.getResourceManager()).registerReloader(HIRESPAINTING_MANAGER);
+        HIRESPAINTING_MANAGER = new HiResPaintingManager(client.getTextureManager());
+        ((ReloadableResourceManagerImpl) client.getResourceManager()).registerReloader(HIRESPAINTING_MANAGER);
     }
 
     @Override
